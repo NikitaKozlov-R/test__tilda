fa00f9caa485d025dfd2b3daf8ce809ce3523261
Nikita Kozlov <kozlov_nikita@outlook.com>
2022-01-29 01:21:26 +0300

    feat: add fetch request to server

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const url = 'https://store.tildacdn.com/api/tgetproduct/'
+
+fetch(url)
+  .then(function (response) {
+    if (!response.ok) {
+      console.log(
+        "Seems like there's a problem. Code Status: " + response.status
+      )
+      return
+    }
+
+    response.json().then(function (data) {
+      dataHandler(data)
+    })
+  })
+  .catch(function (error) {
+    console.log('Fetch Error: ', error)
+  })
