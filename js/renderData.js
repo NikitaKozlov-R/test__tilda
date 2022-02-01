fb08cbfa337dab92a0823af4abfba102502cb311
Nikita Kozlov <kozlov_nikita@outlook.com>
2022-02-02 01:55:14 +0300

    refactor <renderData>: replace one thread with multiple functions

--- js/renderData.js
+++ js/renderData.js
@@ -1,46 +1,52 @@
 function renderData(data) {
-  // CART BUTTON & PRICE
-  const priceOld = data.priceold
-  const priceCurrent = data.price + '₽'
+  renderHeading(data.quantity, data.title, data.descr)
+  renderPrice(data.priceold, data.price)
+  renderImages(data.images, data.title)
+}
 
-  const priceOldContainer = document.querySelector('.c-price__old')
-  const priceCurrentContainer = document.querySelector('.c-price__current')
-
-  dataInner(priceOldContainer, priceOld)
-  dataInner(priceCurrentContainer, priceCurrent)
-
-  // PRODUCT HEADING & DESCRIPTION
-  let quantity = data.quantity
-  const title = data.title
-  const description = data.descr
-
+function renderHeading(quantity, title, description) {
   const quantityContainer = document.querySelector('.c-heading__quantity')
   const titleContainer = document.querySelector('.c-heading__title')
   const descriptionContainer = document.querySelector('.c-heading__description')
 
-  if (quantity < 10 && quantity !== 0) {
+  // If the server return the quantity as a string
+  Number(quantity)
+
+  if (quantity < 10 && quantity) {
     quantity = 'Only ' + quantity + ' left!'
     dataInner(quantityContainer, quantity)
   }
-  if (quantity === 0) {
+  if (!quantity) {
     quantity = 'The product is over!'
     dataInner(quantityContainer, quantity)
   }
 
   dataInner(titleContainer, title)
   dataInner(descriptionContainer, description)
+}
 
-  // IMAGES
-  const imagesArray = JSON.parse(data.images)
+function renderPrice(priceOld, priceCurrent) {
+  const priceOldContainer = document.querySelector('.c-price__old')
+  const priceCurrentContainer = document.querySelector('.c-price__current')
 
+  priceCurrent + '₽'
+
+  dataInner(priceOldContainer, priceOld)
+  dataInner(priceCurrentContainer, priceCurrent)
+}
+
+function renderImages(images, title) {
   const imagesContainer = document.querySelector('.c-slider__frame')
 
-  imagesArray.reverse().forEach(item => {
+  images = JSON.parse(images)
+
+  images.forEach(image => {
     imagesContainer.innerHTML += `
         <img
           class="c-slider__image active"
-          src="${item.img}"
-          alt="Наушники"
+          src="${image.img}"
+          alt="${title}"
+          title="${title}"
         >
       `
   })
