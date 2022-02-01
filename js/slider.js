e8e14cf51aa2bb0bcffde625b33adc7cfe1b7209
Nikita Kozlov <kozlov_nikita@outlook.com>
2022-02-01 06:18:45 +0300

    feat: add slider.js

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const nextButton = document.querySelector('.c-slider__button.next')
+const prevButton = document.querySelector('.c-slider__button.prev')
+const images = document.getElementsByClassName('c-slider__image')
+
+let currentImage = 0
+
+nextButton.addEventListener('click', () => {
+  images[currentImage].classList.remove('active')
+  currentImage++
+
+  if (currentImage === images.length) {
+    currentImage = 0
+  }
+
+  images[currentImage].classList.add('active')
+})
+
+prevButton.addEventListener('click', () => {
+  images[currentImage].classList.remove('active')
+  currentImage--
+
+  if (currentImage < 0) {
+    currentImage = images.length - 1
+  }
+
+  images[currentImage].classList.add('active')
+})
