8468c8ce5efaf45c27285852436d78c76ea2a999
Nikita Kozlov <kozlov_nikita@outlook.com>
2022-02-02 06:30:44 +0300

    refactor: add closure anonymous function with self-call in slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,30 @@
 'use strict'
+;(function () {
+  const nextButton = document.querySelector('.c-slider__button.next')
+  const prevButton = document.querySelector('.c-slider__button.prev')
+  const images = document.getElementsByClassName('c-slider__image')
 
-const nextButton = document.querySelector('.c-slider__button.next')
-const prevButton = document.querySelector('.c-slider__button.prev')
-const images = document.getElementsByClassName('c-slider__image')
+  let currentImage = 0
 
-let currentImage = 0
+  nextButton.addEventListener('click', () => {
+    images[currentImage].classList.remove('active')
+    currentImage++
 
-nextButton.addEventListener('click', () => {
-  images[currentImage].classList.remove('active')
-  currentImage++
+    if (currentImage === images.length) {
+      currentImage = 0
+    }
 
-  if (currentImage === images.length) {
-    currentImage = 0
-  }
+    images[currentImage].classList.add('active')
+  })
 
-  images[currentImage].classList.add('active')
-})
+  prevButton.addEventListener('click', () => {
+    images[currentImage].classList.remove('active')
+    currentImage--
 
-prevButton.addEventListener('click', () => {
-  images[currentImage].classList.remove('active')
-  currentImage--
+    if (currentImage < 0) {
+      currentImage = images.length - 1
+    }
 
-  if (currentImage < 0) {
-    currentImage = images.length - 1
-  }
-
-  images[currentImage].classList.add('active')
-})
+    images[currentImage].classList.add('active')
+  })
+})()
